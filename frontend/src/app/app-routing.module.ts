4d0e0f5253af1bfb9bcc1459e3614fe29b6ba1ad
Mate <mate@koncz.dev>
2021-12-23 17:01:56 +0100

    Add custom font to project (#8)

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InformationFormComponent } from './pages/information-form/information-form.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'form', component: InformationFormComponent },
+  { path: 'details', component: UserProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
