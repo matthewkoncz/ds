a89df89a4f91af1379af88cc24cced58697a8cfa
Mate <mate@koncz.dev>
2021-12-23 17:03:51 +0100

    Add routing for the 2 pages (#5)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { InformationFormComponent } from './pages/information-form/information-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserProfileComponent,
+    InformationFormComponent
   ],
   imports: [
     BrowserModule,
