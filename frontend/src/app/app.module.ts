b4ca77a8fa084267df2ffa63f6f0722ad48bab03
Mate <mate@koncz.dev>
2021-12-26 10:53:30 +0100

    Move Header component to the shared/components folder (#2)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 import { InformationFormComponent } from './pages/information-form/information-form.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
