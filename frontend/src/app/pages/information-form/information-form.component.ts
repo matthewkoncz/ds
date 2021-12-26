6f0d6beba3a9669d19ce177226a8faeb11f86c69
Mate <mate@koncz.dev>
2021-12-26 19:19:32 +0100

    Build settings page with reactive form (#22)

--- frontend/src/app/pages/information-form/information-form.component.ts
+++ frontend/src/app/pages/information-form/information-form.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/shared/services/user.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-information-form',
   templateUrl: './information-form.component.html',
   styleUrls: ['./information-form.component.scss'],
 })
-export class InformationFormComponent implements OnInit {
+export class InformationFormComponent {
+  userForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    birthday: new FormControl(''),
+    about: new FormControl(''),
+    avatar: new FormControl(''),
+  });
+
   constructor(public userService: UserService) {}
 
-  ngOnInit() {
-    this.userService.setData({ firstName: 'Máté' });
+  showPreview = (event: any): void => {
+    const file = event.target.files[0];
+    if (file) {
+      let reader = new FileReader();
+      reader.onload = () => {
+        this.userForm.patchValue({
+          avatar: reader.result as string,
+        });
+        this.userForm.get('avatar')?.updateValueAndValidity();
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  onSubmit() {
+    this.userService.setData(this.userForm.value);
   }
 }
